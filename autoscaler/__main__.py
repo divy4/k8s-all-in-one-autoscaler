506958dc5947d05b40d5d40f69cf20e9842bb84c
Dan Ivy <danivy4@gmail.com>
2025-07-22 17:43:48 -0500

    Humanize suggestions

--- autoscaler/__main__.py
+++ autoscaler/__main__.py
@@ -12,12 +12,17 @@ import autoscaler.util as util
 logger = logging.getLogger(__name__)
 
 PROMETHEUS_URL = "http://k8s.az1.danivy.com:8080/prometheus"
-QUERY_RANGE_SECONDS = 86400
-QUERY_STEP_SECONDS = 15
-CPU_USAGE_PERCENTILE_LOW = 25
-CPU_USAGE_PERCENTILE_HIGH = 75
-MEMORY_USAGE_PERCENTILE_LOW = 25
-MEMORY_USAGE_PERCENTILE_HIGH = 75
+
+# The period and step size, in seconds, to lookup metrics data on
+QUERY_RANGE_SECONDS = 86400 * 7
+QUERY_STEP_SECONDS = 60
+
+# Collection settings
+CPU_PERCENTILES = (25, 75, 100)
+MEMORY_PERCENTILES = (25, 75, 100)
+
+# Algorithm settings
+MEMORY_REQUEST_TARGET_PERCENTAGE = 100
 SUGGESTION_SIGNIFICANT_DIGITS = 2
 
 
@@ -29,6 +34,7 @@ def autoscale_resources() -> None:
     )
     data = collect_metrics(collector)
     suggestions = suggest_resources(data)
+    humanize_suggestions(suggestions)
     print(json.dumps(suggestions, indent=2))
 
 
@@ -42,31 +48,75 @@ def suggest_resources(
     logger.info("Generating resource suggestions...")
     suggestions = collections.defaultdict(lambda: collections.defaultdict(dict))
     for container, metrics in data.items():
+        # CPU requests
+        if "cpu_usage_percentile" in metrics:
+            suggestions[str(container)]["requests"]["cpu"] = (
+                metrics["cpu_usage_percentile"][25]
+                + metrics["cpu_usage_percentile"][75]
+            ) / 2
         # Memory requests
-        if "memory_usage_low" in metrics and "memory_usage_high" in metrics:
-            suggestion = (
-                metrics["memory_usage_low"] + metrics["memory_usage_low"]
-            ) / 2.0
-            suggestions[str(container)]["requests"]["memory"] = humanize_memory(
-                suggestion
-            )
+        if "memory_usage_percentile" in metrics:
+            suggestions[str(container)]["requests"]["memory"] = (
+                metrics["memory_usage_percentile"][25]
+                + metrics["memory_usage_percentile"][75]
+            ) / 2
     return suggestions
 
 
-def humanize_memory(value: float) -> str:
-    """Humanizes a memory value."""
-    value = int(value)
-    if value < 1024:
-        return str(value)
-    elif value < 1024**2:
-        return f"{ceil_sigfig(value / 1024, SUGGESTION_SIGNIFICANT_DIGITS)}Ki"
-    elif value < 1024**3:
-        return f"{ceil_sigfig(value / 1024 ** 2, SUGGESTION_SIGNIFICANT_DIGITS)}Mi"
+def humanize_suggestions(
+    suggestions: Dict[util.Container, Any],
+) -> Dict[util.Container, Any]:
+    """Reformats suggestions into Kubernetes human-readable values, e.g. 10Mi,
+    123m, 1.7Gi."""
+    for suggestion in suggestions.values():
+        if "requests" in suggestion:
+            if "cpu" in suggestion["requests"]:
+                suggestion["requests"]["cpu"] = humanize_cpu(
+                    suggestion["requests"]["cpu"]
+                )
+            if "memory" in suggestion["requests"]:
+                suggestion["requests"]["memory"] = humanize_memory(
+                    suggestion["requests"]["memory"]
+                )
+        if "limits" in suggestion:
+            if "cpu" in suggestion["requests"]:
+                suggestion["requests"]["cpu"] = humanize_cpu(
+                    suggestion["requests"]["cpu"]
+                )
+            if "memory" in suggestion["limits"]:
+                suggestion["limits"]["memory"] = humanize_memory(
+                    suggestion["limits"]["memory"]
+                )
+
+
+def humanize_cpu(x: float) -> str:
+    """Humanizes a cpu value."""
+    if x <= 0:
+        raise ValueError(f"Unable to convert '{x}' to a Kubernetes cpu value.")
+    elif x < 0.001:
+        return "1m"
+    elif x < 1:
+        return f"{advanced_ceil(x * 1000, SUGGESTION_SIGNIFICANT_DIGITS, 0)}m"
     else:
-        return f"{ceil_sigfig(value / 1024 ** 3, SUGGESTION_SIGNIFICANT_DIGITS)}Gi"
+        return str(advanced_ceil(x, SUGGESTION_SIGNIFICANT_DIGITS))
 
 
-def ceil_sigfig(x: float, sigfigs: int) -> str:
+def humanize_memory(x: float) -> str:
+    """Humanizes a memory value."""
+    x = int(x)
+    if x <= 0:
+        raise ValueError(f"Unable to convert '{x}' to a Kubernetes memory value.")
+    elif x < 1024:
+        return str(advanced_ceil(x, SUGGESTION_SIGNIFICANT_DIGITS))
+    elif x < 1024**2:
+        return f"{advanced_ceil(x / 1024, SUGGESTION_SIGNIFICANT_DIGITS)}Ki"
+    elif x < 1024**3:
+        return f"{advanced_ceil(x / 1024 ** 2, SUGGESTION_SIGNIFICANT_DIGITS)}Mi"
+    else:
+        return f"{advanced_ceil(x / 1024 ** 3, SUGGESTION_SIGNIFICANT_DIGITS)}Gi"
+
+
+def advanced_ceil(x: float, sigfigs: int, decimals: int = None) -> int | float:
     """Computes ceil a number of significant digits."""
     # This function could handle non-positive values with some extra work.
     # But we shouldn't expect them anyway, so throw an error instead.
@@ -76,11 +126,14 @@ def ceil_sigfig(x: float, sigfigs: int) -> str:
     first_sigfig_place = int(numpy.floor(numpy.log10(x)))
     # Convert number of significant digits to what precision to round on
     precision = sigfigs - 1 - first_sigfig_place
+    # If decimals would be less precise than significant figures, use it instead.
+    if decimals is not None and decimals < precision:
+        precision = decimals
     # Numpy and math's ceil functions don't allow setting precision...
     # ...so we have to do it ourselves *shakes fist*
     x = numpy.true_divide(math.ceil(x * 10**precision), 10**precision)
     # Convert to int if there's no decimal places
-    if first_sigfig_place + 1 >= sigfigs:
+    if precision <= 0:
         x = int(x)
     return x
 
@@ -90,7 +143,7 @@ def ceil_sigfig(x: float, sigfigs: int) -> str:
 
 def collect_metrics(
     collector: metrics_collector.MetricsCollector,
-) -> Dict[Any, Dict[str, float]]:
+) -> Dict[Any, Dict[str, Any]]:
     """Collects metrics for all containers in a cluster."""
     # Collect data
     data = collections.defaultdict(dict)
@@ -99,20 +152,10 @@ def collect_metrics(
     compute_max(data, "cpu_throttle_time", metrics)
     # CPU usage
     metrics = collector.get_cpu_usage()
-    compute_percentile(
-        data,
-        ("cpu_usage_low", "cpu_usage_high"),
-        (CPU_USAGE_PERCENTILE_LOW, CPU_USAGE_PERCENTILE_HIGH),
-        metrics,
-    )
+    compute_percentile(data, "cpu_usage_percentile", CPU_PERCENTILES, metrics)
     # Memory usage
     metrics = collector.get_memory_usage()
-    compute_percentile(
-        data,
-        ("memory_usage_low", "memory_usage_high"),
-        (MEMORY_USAGE_PERCENTILE_LOW, MEMORY_USAGE_PERCENTILE_HIGH),
-        metrics,
-    )
+    compute_percentile(data, "memory_usage_percentile", MEMORY_PERCENTILES, metrics)
     # Out of memory kills
     metrics = collector.get_out_of_memory_kills()
     compute_max(data, "out_of_memory_kills", metrics)
@@ -121,7 +164,7 @@ def collect_metrics(
 
 def compute_max(
     data: Dict[
-        Any, Dict[str, Dict[int, float]]
+        Any, Dict[str, Dict[int, Any]]
     ],  # The collection of all metrics to append data to.
     key: str,  # The subkey to store the metrics in.
     metrics: metrics_collector.ContainerMetrics,  # A set of container metrics to evaluate.
@@ -134,18 +177,21 @@ def compute_max(
 
 def compute_percentile(
     data: Dict[
-        Any, Dict[str, Dict[int, float]]
+        Any, Dict[str, Dict[int, Any]]
     ],  # The collection of all metrics to append data to.
-    keys: Iterable[str],  # The subkeys to store the metrics in.
+    key: str,  # The subkey to store the metrics in.
     percentiles: Iterable[int],  # What percentiles to collect.
     metrics: metrics_collector.ContainerMetrics,  # A set of container metrics to evaluate.
 ) -> None:
     """Compute a set of percentiles on a set of metrics and add them to
-    data[container][key]."""
+    data[container][key][percentile]."""
     for container, metric in metrics.items():
+        # Ensure dict exists under the key
+        data[container][key] = collections.defaultdict(dict)
+        # Compute all percentiles
         values = numpy.percentile(metric, percentiles)
-        for key, value in zip(keys, values):
-            data[container][key] = value
+        for percentile, value in zip(percentiles, values):
+            data[container][key][percentile] = value
 
 
 if __name__ == "__main__":
